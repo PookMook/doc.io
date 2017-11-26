12ba37c24d97c5e3aefca45a15ab6e517f3b568d
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-11-26 18:36:20 -0500

    objectify

--- app/helpers/fullList.js
+++ app/helpers/fullList.js
@@ -1,2 +1,11 @@
-const fullList = [ 'cake', 'brioche' ];
+const fullList = [
+    {
+        title: 'cake',
+        id: 0
+    },
+    {
+        title: 'brioche',
+        id: 1
+    }
+];
 export default fullList;
