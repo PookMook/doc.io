a28cbbbb3691f8450b7e8862c654914fcfc6489c
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-12-01 18:59:19 -0500

    displayItem styling

--- app/components/DisplayItem.js
+++ app/components/DisplayItem.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import store from '../store/store';
-
+import Marked from 'marked';
+import { displayItem } from '../styles/displayItem.scss';
 const Fragment = React.Fragment;
 
 export default class DisplayItem extends Component {
@@ -19,14 +20,15 @@ export default class DisplayItem extends Component {
     render() {
         const documentation = store.getState().documentation;
         return (
-          <div >
+          <div className={displayItem}>
           {documentation.itemById && documentation.itemById[this.state.item] &&
               <Fragment>
                   <nav>
                       <Link to="/">documentation</Link>
                        > <Link to={'/category/' + this.state.category}>{documentation.categoryById[this.state.category].title}</Link> > <Link to={'/category/' + this.state.category + '/item/' + this.state.item} >{documentation.itemById[this.state.item].title}</Link>
                   </nav>
-                  <p>{documentation.itemById[this.state.item].desc}</p>
+                  <h1 className="headline">{documentation.itemById[this.state.item].title}</h1>
+                  <section dangerouslySetInnerHTML={{__html: Marked(documentation.itemById[this.state.item].desc)}} />
               </Fragment>
           }
           </div>
