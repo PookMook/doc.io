ec39c24e7d9a936b954430cef1f53881c4ce0a13
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-12-24 14:41:38 -0500

    transition to state

--- app/components/Documentation.js
+++ app/components/Documentation.js
@@ -1,39 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { doc } from '../styles/doc.scss';
 import Category from './Documentation/Category';
-import store from '../store/store';
 import doubleDigit from '../helpers/doubleDigit';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-export default class Documentation extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = { filter: store.getState().documentation.filter,  loaded: ( store.getState().documentation.compiledAt ? true : false )};
-        this.handleFilter = this.handleFilter.bind(this);
-    }
-
-
-    handleFilter(e) {
-        this.setState({ filter: e.target.value});
-        this.props.searchItem(e.target.value);
-    }
-
-    render() {
-        const documentation = this.props.state;
-        return (
-          <div className={doc}>
-            <input value={this.state.filter} onChange={this.handleFilter} placeholder="Search" />
-            {!documentation.loaded && <p>Loading...</p>}
-            {documentation.loaded &&
-            <p>Last update {doubleDigit(documentation.compiledAt.getDate())}/{doubleDigit(documentation.compiledAt.getMonth())} {doubleDigit(documentation.compiledAt.getHours())}:{doubleDigit(documentation.compiledAt.getMinutes())}</p>}
-            <Link  to="/">{documentation.title}</Link>
-            {this.state.loaded && documentation.filteredList.map((item) => (<Category {...item} key={'Category' + item.id} css={doc}/>))}
-            <p>{this.props.state.filteredList && JSON.stringify(this.props.state.filteredList)}</p>
-          </div>
-        );
-    }
+export default function Documentation(props) {
+    const documentation = props.state;
+    return (
+      <div className={doc}>
+        <input value={documentation.filter}
+          onChange={(e)=>props.searchItem(e.target.value)} placeholder="Search" />
+        {!documentation.loaded && <p>Loading...</p>}
+        {documentation.loaded &&
+        <p>Last update {doubleDigit(documentation.compiledAt.getDate())}/{doubleDigit(documentation.compiledAt.getMonth())} {doubleDigit(documentation.compiledAt.getHours())}:{doubleDigit(documentation.compiledAt.getMinutes())}</p>}
+        <Link  to="/">{documentation.title}</Link>
+        {documentation.loaded && documentation.filteredList.map((item) => (<Category {...item} key={'Category' + item.id} css={doc}/>))}
+      </div>
+    );
 }
 
 Documentation.propTypes = {
